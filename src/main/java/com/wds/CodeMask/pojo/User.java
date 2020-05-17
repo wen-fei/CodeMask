d17e83b350a7d75782650d162293d1267a2021ad
wen-fei <282699766@qq.com>
2020-05-17 10:06:28 +0800

    pojo: user, add necessary annotation

---
+++ src/main/java/com/wds/CodeMask/pojo/User.java
@@ -0,0 +1,42 @@
+package com.wds.CodeMask.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author : TenYun
+ * @date : 2020-05-17 9:55
+ * @description : user key information
+ * use for login
+ * user detail information see userinfo
+ *
+ **/
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+}
