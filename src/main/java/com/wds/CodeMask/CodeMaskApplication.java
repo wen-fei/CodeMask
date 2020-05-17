0f7a83e83082bf62b1f6c45cc6d44750088b0bc1
wen-fei <282699766@qq.com>
2020-05-17 09:40:02 +0800

    CodeMask Starter

---
+++ src/main/java/com/wds/CodeMask/CodeMaskApplication.java
@@ -0,0 +1,13 @@
+package com.wds.CodeMask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMaskApplication.class, args);
+	}
+
+}
